bd06d404e5b69f8f083e82fa80c266ef1e78436c
connection-lost <frankwu1995@hotmail.com>
2015-06-07 11:50:14 -0400

    Basic make a gun

--- src/me/crafter/mc/splatoon/Splatoon.java
+++ src/me/crafter/mc/splatoon/Splatoon.java
@@ -1,6 +1,10 @@
 package me.crafter.mc.splatoon;
 
 import java.util.logging.Logger;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -22,5 +26,44 @@ public class Splatoon extends JavaPlugin {
     public void onDisable() {
     }
 
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, final String[] args){
+    	if (cmd.getName().equals("splatoon")){
+    		// Not require player
+    		
+    		
+    		
+    		
+    		
+    		
+    		
+    		// Requires player
+    		if (!(sender instanceof Player)) return true;
+    		Player player = (Player)sender;
+    		if (args.length == 0){
+    			// TODO show help
+    		} else {
+    			if (args[0].equals("make")){
+    				try {
+    					int color = Integer.parseInt(args[1]);
+    					int power = Integer.parseInt(args[2]);
+    					int firerate = Integer.parseInt(args[3]);
+    					int bulletspeed = Integer.parseInt(args[4]);
+    					int accuracy = Integer.parseInt(args[5]);
+    					SplatterShot.makeGun(player, color, power, firerate, bulletspeed, accuracy);
+    					player.sendMessage("Made a gun.");
+    				} catch (Exception ex) {
+    					player.sendMessage("Something wrong");
+    				}
+    			}
+    			
+    			
+    		}
+    		
+    		
+    		
+    		
+    	}
+    	return true;
+    }
 	
 }
--- src/me/crafter/mc/splatoon/SplatterShot.java
+++ src/me/crafter/mc/splatoon/SplatterShot.java
@@ -1,18 +1,24 @@
 package me.crafter.mc.splatoon;
 
+import java.util.LinkedList;
 import java.util.List;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 public class SplatterShot {
 
+	private int color;
 	private int power;
 	private int firerate;
 	private int bulletspeed;
 	private int accuracy;
 	
-	public SplatterShot(int _power, int _firerate, int _bulletspeed, int _accuracy){
+	public SplatterShot(int _color, int _power, int _firerate, int _bulletspeed, int _accuracy){
+		color = _color;
 		power = _power;
 		firerate = _firerate;
 		bulletspeed = _bulletspeed;
@@ -31,13 +37,37 @@ public class SplatterShot {
 		ItemMeta itemmeta = item.getItemMeta();
 		try {
 			List<String> lore = itemmeta.getLore();
-			int power = Integer.parseInt(lore.get(0).split(" ")[1]);
-			int firerate = Integer.parseInt(lore.get(1).split(" ")[1]);
-			int bulletspeed = Integer.parseInt(lore.get(2).split(" ")[1]);
-			int accuracy = Integer.parseInt(lore.get(3).split(" ")[1]);
-			return new SplatterShot(power, firerate, bulletspeed, accuracy);
+			int color = Integer.parseInt(lore.get(0).split(" ")[1]);
+			int power = Integer.parseInt(lore.get(1).split(" ")[1]);
+			int firerate = Integer.parseInt(lore.get(2).split(" ")[1]);
+			int bulletspeed = Integer.parseInt(lore.get(3).split(" ")[1]);
+			int accuracy = Integer.parseInt(lore.get(4).split(" ")[1]);
+			return new SplatterShot(color, power, firerate, bulletspeed, accuracy);
 		} catch (Exception ex) {}
 		return null;
+	}
+	
+	public static void makeGun(Player player, int color, int power, int firerate, int bulletspeed, int accuracy){
+		ItemStack item = player.getItemInHand();
+		if (item == null || item.getType().equals(Material.AIR)){
+			player.sendMessage(ChatColor.RED + "Not a correct item.");
+		} else {
+			player.setItemInHand(makeGun(player.getItemInHand(), color, power, firerate, bulletspeed, accuracy));
+		}
+		
+	}
+	
+	public static ItemStack makeGun(ItemStack original, int color, int power, int firerate, int bulletspeed, int accuracy){
+		ItemMeta itemmeta = original.getItemMeta();
+		List<String> lore = new LinkedList<String>();
+		lore.add(ChatColor.WHITE + "Color: " + color);
+		lore.add(ChatColor.WHITE + "Power: " + power);
+		lore.add(ChatColor.WHITE + "Fire Rate: " + firerate);
+		lore.add(ChatColor.WHITE + "Projectile Speed: " + bulletspeed);
+		lore.add(ChatColor.WHITE + "Accuracy: " + accuracy);
+		itemmeta.setLore(lore);
+		original.setItemMeta(itemmeta);
+		return original;
 	}
 	
 }
